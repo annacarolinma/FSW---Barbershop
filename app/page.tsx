99012c9b3cfc0a2499783529f9e34888a0bc4bda
annacarolinmae <carolinamorellichaves@gmail.com>
2024-08-10 14:36:32 -0300

    style:header&page

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ const Home = () => {
     <div>
       <Header />
 
-      {/*cabeçalho d boas vindas  data*/}
-      <div className="p-20">
+      {/*cabeçalho d boas vindas com a data*/}
+      <div className="ml-12 p-20">
         <h2 className="gap-8 text-xl font-bold">Olá, Ana!</h2>
         <p>Segunda-feira, 07 de agosto</p>
 
-        {/* Barra de psquisa e filtragem */}
+        {/* Barra de pesquisa e filtragem */}
         <div className="mt-6 flex items-start gap-2">
-          <Input placeholder="Faça uma busca" className="w-306 h-10 p-2" />
+          <Input placeholder="Faça uma busca" className="h-10 w-96 p-2" />
           <Button className="rounded-md text-white">
             <SearchIcon />
           </Button>
